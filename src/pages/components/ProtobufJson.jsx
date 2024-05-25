afda19a4d045d09f4ef32f843eb9352f122e7b07
Snofly <49230243+0110wdj@users.noreply.github.com>
2024-05-25 16:58:03 +0800

    feat(shell) protobuf 结构修改

--- src/pages/components/ProtobufJson.jsx
+++ src/pages/components/ProtobufJson.jsx
@@ -1,9 +1,13 @@
-import { Button,Input } from 'antd'
+import { Button, Input, Select, message } from 'antd'
 import React, { useState } from 'react'
+import { protbufDecode } from './lib/potobuf'
+
+const Option = Select.Option
 
 const ProtobufJson = () => {
   const [base64, setBase64] = useState('')
   const [josnStr, setJSONStr] = useState('')
+  const [protobufType, setProtobufType] = useState('Events')
 
   return (
     <>
@@ -15,42 +19,45 @@ const ProtobufJson = () => {
           placeholder='base64'
         />
         <div>
+          <Select
+            defaultValue='Events'
+            style={{
+              width: 120,
+            }}
+            onSelect={v => setProtobufType(v)}
+            value={protobufType}
+            options={[
+              'Chart',
+              'Charts',
+              'MonitorStatus',
+              'RoundTick',
+              'Problem',
+              'Event',
+              'Events',
+            ].map(i => ({ value: i, label: i }))}
+          />
           <Button
-            onClick={async () => {
-              // try {
-              //   const res = await unpack_patch(base64)
-              //   setJSONStr(JSON.stringify(res))
-              //   message.info('解码成功，按 F12 后在控制台查看详情！')
-              //   console.log(res)
-              // } catch (error) {
-              //   message.error('解码失败！')
-              //   console.error(error)
-              // }
+            onClick={() => {
+              try {
+                const res = protbufDecode(base64, protobufType)
+                setJSONStr(JSON.stringify(res))
+                message.info('解码成功，按 F12 后在控制台查看详情！')
+                console.log(res)
+              } catch (error) {
+                message.error('解码失败！')
+                console.error(error)
+              }
             }}
             style={{ margin: '0 10px' }}
           >
             解码
-          </Button>
-          <Button
-            onClick={async () => {
-              // try {
-              //   const res = await pack(JSON.parse(josnStr))
-              //   setBase64(res)
-              //   message.info('编码成功！')
-              // } catch (error) {
-              //   message.error('编码失败！')
-              //   console.error(error)
-              // }
-            }}
-            style={{ margin: '0 10px' }}
-          >
-            编码
           </Button>
         </div>
         <Input.TextArea
           value={josnStr}
           onChange={e => setJSONStr(e.target.value)}
           placeholder='json'
+          disabled
         />
       </div>
     </>
