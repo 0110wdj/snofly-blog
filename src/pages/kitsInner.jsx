31a6a3f62916b905e0e50b4872c89b99655ac06c
Snofly <49230243+0110wdj@users.noreply.github.com>
2024-05-25 17:36:08 +0800

    feat(kits) 工具箱增加显示隐藏功能

--- src/pages/kitsInner.jsx
+++ src/pages/kitsInner.jsx
@@ -1,15 +1,47 @@
+import { Select } from 'antd'
+import { useState } from 'react'
 import Craw from './components/Craw'
 import Hex from './components/Hex'
 import PackJson from './components/PackJson'
 import ProtobufJson from './components/ProtobufJson'
 
+const options = [
+  {
+    value: 'protobuf',
+    label: 'protobuf 解码',
+  },
+  {
+    value: 'packjson',
+    label: 'packjson 解码与编码',
+  },
+  {
+    value: 'hex',
+    label: '进制转换',
+  },
+  {
+    value: 'craw',
+    label: '爬虫组件',
+  },
+]
+
 export default function Home() {
+  const [selected, setSelected] = useState(['protobuf', 'packjson'])
   return (
     <div class='flex flex-wrap flex-col items-center'>
-      <ProtobufJson />
-      <Hex />
-      <PackJson />
-      <Craw />
+      <h2>选择功能组件（请在 light 主题下使用）</h2>
+      <Select
+        mode='multiple'
+        value={selected}
+        style={{
+          width: '100%',
+        }}
+        options={options}
+        onChange={v => setSelected(v)}
+      />
+      {selected.includes('protobuf') && <ProtobufJson />}
+      {selected.includes('hex') && <Hex />}
+      {selected.includes('packjson') && <PackJson />}
+      {selected.includes('craw') && <Craw />}
     </div>
   )
 }
