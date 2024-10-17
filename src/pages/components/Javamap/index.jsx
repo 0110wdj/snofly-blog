f3ba16c7bb2de6059ab8626bb8d6670b0b710783
Snofly <49230243+0110wdj@users.noreply.github.com>
2024-10-17 16:38:04 +0800

    javamap 样式修改

--- src/pages/components/Javamap/index.jsx
+++ src/pages/components/Javamap/index.jsx
@@ -4,7 +4,12 @@ const Javamap = () => {
       <iframe
         src="/files/javamap/javamap.html"
         title="戴 java cb"
-        style={{ width: "800px", height: "2400px", border: "1px solid #yellow" }}
+        style={{
+          width: "800px",
+          height: "2400px",
+          border: "1px solid red",
+          background: "oklch(0.8 0.06 249.89)",
+        }}
       />
     </div>
   );
